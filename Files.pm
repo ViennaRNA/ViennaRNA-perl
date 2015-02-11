3e781cb82d0eaf83e02cd7c6f633113697175379
Ronny Lorenz <ronny@tbi.univie.ac.at>
2015-02-11 23:04:02 +0100

    Initial commit

---
+++ Files.pm
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+#
+package ViennaRNA::Files;
+
+use strict;
+use Exporter;
+use warnings;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(read_dotplot);
+%EXPORT_TAGS = ();
+
+
+
+
+sub read_dotplot{
+  my $dotplot = shift;
+  my %mfe;
+  my %probs;
+  my $seq;
+  my $seq_start;
+
+  open(DOT, "<".$dotplot);
+  while(<DOT>){
+    if(defined($seq_start)){
+      undef($seq_start), next  unless /^([AUCGTNaucgtn]+)/;
+      $seq .= $1;
+    }
+
+    $seq_start = 1 if /^\/sequence/;
+
+    next unless /(\d+)\s+(\d+)\s+([0-9.Ee-]+)\s+(ubox|lbox)/;
+    my ($i, $j, $p) = ($1,$2,$3);
+    if($4 eq "ubox"){
+      $p *= $p;
+      $probs{$i,$j} = $p;
+    } else {
+      $mfe{$i,$j} = 1;
+    }
+  }
+  close(DOT);
+
+  return ($seq, \%mfe, \%probs);
+}
+
+1;
---
+++ Reliability.pm
@@ -0,0 +1,103 @@
+#!/usr/bin/perl
+#
+package ViennaRNA::Reliability;
+
+use strict;
+use Exporter;
+use warnings;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(rel_posent rel_prob rel_access);
+%EXPORT_TAGS = ();
+
+
+
+sub rel_posent{
+  my @pp;
+  my @sp;
+  my $log2  = log(2);
+  my $probs = shift;
+  my $n     = shift;
+
+  for my $i (1..$n){
+    for my $j (($i+1)..$n){
+      next unless exists $probs->{$i,$j};
+      my $p  = $probs->{$i,$j};
+
+      my $ss = ($p>0)?$p*log($p):0;
+      $sp[$i] += $ss;
+      $sp[$j] += $ss;
+      $pp[$i] += $p;
+      $pp[$j] += $p;
+    }
+  }
+
+  for my $i (1..$n) {
+    no warnings;  # $p[$i] may be undef
+    $sp[$i] += ($pp[$i] < 1) ? (1 - $pp[$i]) * log(1 - $pp[$i]) : 0;
+    $sp[$i] /= -$log2;
+  }
+
+  shift @sp; # get rid of [0] entry
+
+  return \@sp;
+}
+
+sub rel_probs{
+  my $probs = shift;
+  my $mfe   = shift;
+  my $n     = shift;
+  my @pp;
+  my @sp;
+  for my $i (1..$n){
+    for my $j (($i+1)..$n){
+      next unless exists $probs->{$i,$j};
+      my $p  = $probs->{$i,$j};
+
+      $sp[$i] = $sp[$j] = $p if exists  $mfe->{$i,$j};
+
+      $pp[$i] += $p;
+      $pp[$j] += $p;
+    }
+
+    no warnings;  # $p[$i] may be undef
+    $sp[$i] = 1-$pp[$i] if !defined $sp[$i];
+  }
+
+  shift @sp; # get rid of [0] entry
+
+  return \@sp;
+}
+
+sub rel_access {
+  my $probs = shift;
+  my $mfe   = shift;
+  my $n     = shift;
+  my @pp;
+  my @sp;
+  for my $i (1..$n){
+    for my $j (($i+1)..$n){
+      next unless exists $probs->{$i,$j};
+      my $p  = $probs->{$i,$j};
+
+      $sp[$i] = $sp[$j] = 1-$p if exists $mfe->{$i,$j};
+
+      $pp[$i] += $p;
+      $pp[$j] += $p;
+    }
+
+    no warnings;  # $p[$i] may be undef
+    $sp[$i] = 1-$pp[$i] if !defined $sp[$i];
+  }
+
+  shift @sp; # get rid of [0] entry
+
+  return \@sp;
+}
+
+
+1;
